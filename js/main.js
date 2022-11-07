183282548f45c164da009b1343d01031ac6e533e
mimaraka <nj.ot.niconico@gmail.com>
2022-11-07 20:16:45 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, window, location, CSInterface, SystemPath, themeManager*/
+
+function absolutePath(path) {
+    var e = document.createElement('span');
+    e.innerHTML = '<a href="' + path + '" />';
+    return e.firstChild.href;
+}
+
+
+function loadGif(path) {
+    var image = document.getElementById("gifimage");
+    image.src = path;
+}
+
+
+(function () {
+    'use strict';
+
+    var csInterface = new CSInterface();
+    
+    
+    function init() {
+        try {
+            themeManager.init();
+
+            csInterface.evalScript('initialize("' + absolutePath("./") + '")', function (init_path) {
+                if (init_path != false) {
+                    loadGif(init_path);
+                }
+            });
+
+
+            $("#content").dblclick(function () {
+                csInterface.evalScript('openGif()', function (result) {
+                    if (result != false) {
+                        loadGif(result);
+                        csInterface.evalScript('savePath("' + result + '","' + absolutePath("./") + '")');
+                    }
+                });
+            });
+        }
+        catch (e) {
+            alert(e);
+        }
+    }
+
+    init();
+
+}());
---
+++ jsx/hostscript.jsx
@@ -0,0 +1,37 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, Folder*/
+
+
+function savePath(gif_path, script_dir) {
+    var file = new File(script_dir + "gif_path.txt");
+
+    file.open("w");
+    file.write(gif_path);
+    file.close();
+}
+
+
+function initialize(script_dir) {
+    var file = new File(script_dir + "gif_path.txt");
+
+    if(file.exists == true){
+        file.open("r");
+        var path = file.read();
+        file.close();
+        if (path != "") {
+            return path;
+        }
+    }
+    return false;
+}
+
+
+function openGif() {
+    var path = File.openDialog("Select a GIF file", "GIF animation:*.gif").fsName.replace(/\\/g, "\\\\");
+    if (path != "") {
+        return path;
+    }
+    else {
+        return false;
+    }
+}
